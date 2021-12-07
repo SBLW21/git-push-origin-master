0c0a45b8a13115b2d012a3b3c99aff2ae5d7814f
sophie blancard <sblanchard.lw@gmail.com>
2021-12-07 17:00:02 +0100

    unfinished exercice_view edit, create and links

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,50 @@
+class ArticlesController < ApplicationController
+
+  before_action :set_article, except: [:index, :new, :create]
+  def index
+    @articles = Article.all
+  end
+
+  def show
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @aticle = Article.new(article_params)
+      if @article.save
+        redirect_to articles_path
+      else
+        render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @article.update(article_params)
+      if @article.save
+        redirect_to edit_article_path
+      else
+        render :edit
+    end
+  end
+
+  def destroy
+    @article.destroy
+      redirect_to articles_path
+  end
+
+private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+end
